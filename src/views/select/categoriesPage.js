15f8fc4caeb2a5343d273db83338435ad96704d4
Elizabeth Lee <elizabethlee245@gmail.com>
2022-12-13 23:27:10 -0500

    commit

---
+++ src/views/select/categoriesPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import '../../categoriesPage.css';
+//import  from './';
+
+class CategoriesPage extends Component {
+  
+  render() {
+    return (
+      <div CategoriesPage-container>
+        <div className="header-top">
+          <div className="title">what do you have?</div>
+          <div className="subtitle">select a category</div>
+        </div>
+
+        
+      </div>
+    );
+
+    
+  }
+}
+
+export default CategoriesPage
